dacabed522876d8769c264924c0c4078adb3ff6c
sunghyun <sunghyun1148@gmail.com>
2025-05-02 17:37:12 +0900

    portfolio 페이지지 생성

--- app/projects/my-portfolio/page.tsx
+++ app/projects/my-portfolio/page.tsx
@@ -1,5 +1,19 @@
 function Page() {
-  return <div className="h-full">test</div>;
+  return (
+    <div className="h-full">
+      <h1 className="text-3xl font-bold">My Portfolio</h1>
+      <p className="text-lg">개인 포트폴리오 웹사이트입니다.</p>
+      <p className="text-lg">React, Next.js, Tailwind CSS로 제작되었습니다.</p>
+
+      <ul>
+        <li>다크모드</li>
+        <li>동적인 랜딩 페이지</li>
+        <li>플로팅 네비게이션바</li>
+        <li>top 버튼</li>
+        <li>반응형</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Page;
