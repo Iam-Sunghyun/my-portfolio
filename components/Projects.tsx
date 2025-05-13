80ef410374e57f58cbdb5c2a1d2c2ab9add96d1e
sunghyun <sunghyun1148@gmail.com>
2025-05-13 17:13:54 +0900

    Project 컴포넌트 추가 + motion 추가

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,15 +4,51 @@ import Link from "next/link";
 import Card from "./ui/Card";
 import HorizentalLine from "./ui/HorizentalLine";
 import ItemList from "./ui/ItemList";
+import Project from "./ui/Project";
+import { motion, Variants } from "framer-motion";
+
+const fadeInAnimationVariants: Variants = {
+  initial: {
+    opacity: 0,
+    scale: 0.2,
+    y: 200,
+  },
+  animate: {
+    opacity: 1,
+    scale: 1,
+    y: 0,
+  },
+};
 
 const Projects = () => {
   return (
-    <div id="Projects">
+    <section id="Projects">
       <HorizentalLine text="Projects" />
-      <div className="flex flex-col items-center gap-10 text-[1.1rem] mb-24">
-        {projects.map((project) => (
+      <div
+        // className="flex flex-col items-center gap-10 text-[1.1rem] mb-24"
+        className="flex flex-col gap-8 mt-20 items-center"
+      >
+        {projects.map((project, index) => (
           <Link key={project.name} href={`${project.link}`}>
-            <Card className="py-8 px-12 hover:border-blue-400 flex flex-col lg:flex-row lg:justify-between ">
+            <motion.div
+              variants={fadeInAnimationVariants}
+              initial="initial"
+              whileInView={"animate"}
+              // viewport={{ once: true }}
+              transition={{
+                duration: 0.5,
+                delay: index * 0.2,
+                // type: "spring",
+              }}
+            >
+              <Project
+                name={project.name}
+                skills={project.skills}
+                image={project.image}
+                description={project.description}
+                link={project.link}
+              />
+              {/* <Card className="py-8 px-12 hover:border-blue-400 flex flex-col lg:flex-row lg:justify-between ">
               <div className="flex flex-col justify-center">
                 <h1 className="mb-5">{project.name}</h1>
                 <Image
@@ -36,11 +72,12 @@ const Projects = () => {
                   </ul>
                 </div>
               </div>
-            </Card>
+            </Card> */}
+            </motion.div>
           </Link>
         ))}
       </div>
-    </div>
+    </section>
   );
 };
 
